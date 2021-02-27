1ca7c8efd5e0ad218991e5a35ac8b081f26b97ca
gaspid <gaspid194@gmail.com>
2021-02-27 10:31:42 +0300

    feat: implement task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  return members.reduce((matt, Name) => (
+    (typeof Name === 'string') ?
+      matt + Name.trim().slice(0, 1) :
+      matt
+  ), "")
+    .toUpperCase()
+    .split('')
+    .sort()
+    .join('')
 };
